9c05ffff1cd80995088d42cabee45a002e4b5a85
Alessandro Mazzoleni <alemazzo95@gmail.com>
2022-02-13 19:39:50 +0100

    Fixed format of returned obj of asymKeyAgree method

--- crypto/crypto_driver.js
+++ crypto/crypto_driver.js
@@ -18,7 +18,7 @@ module.exports = {
  * Returns the DH shared secret. If the aliceKeyPair is not passed, a randomly one is generated and returned.
  * @param {string} bobPubKeyExport the exported bob public key
  * @param {crypto.KeyPairKeyObjectResult} aliceKeyPair the alice key pair. If null it will be randomly generated.
- * @returns an object with the following fields: "keyAgree" a {@link Buffer} representing the DH shared secret, "aliceKeyPair" a {@link crypto.KeyPairKeyObjectResult}, "alicePubKeyExport" a string representing the exported alice public key
+ * @returns an object with the following fields: "sharedSecret" a {@link Buffer} representing the DH shared secret, "keyPair" the alice key pair ({@link crypto.KeyPairKeyObjectResult}), "pubKeyExport" a string representing the exported alice public key
  */
 function asymKeyAgree(bobPubKeyExport, aliceKeyPair = null) {
     if (aliceKeyPair == null) {
@@ -27,9 +27,9 @@ function asymKeyAgree(bobPubKeyExport, aliceKeyPair = null) {
     let bobPubKey = cc.importPubKey(bobPubKeyExport);
     let keyAgree = cc.x25519KeyAgree(aliceKeyPair.privateKey, bobPubKey);
     return {
-        keyAgree: keyAgree,
-        aliceKeyPair: aliceKeyPair,
-        alicePubKeyExport: cc.exportPubKey(aliceKeyPair.publicKey)
+        sharedSecret: keyAgree,
+        keyPair: aliceKeyPair,
+        pubKeyExport: cc.exportPubKey(aliceKeyPair.publicKey)
     };
 }
 
